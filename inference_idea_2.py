2cb971ebe00829e742f01e70ab65769dd6690250
Satvik Dixit <78974280+satvik-dixit@users.noreply.github.com>
2024-10-30 19:47:49 -0400

    Update inference_idea_2.py

--- inference_idea_2.py
+++ inference_idea_2.py
@@ -39,7 +39,7 @@ def generate_samples(target_event, class_idx1, class_idx2, sampler, cond_scale, 
     
     return gen_audio_1, gen_audio_2
 
-def save_samples(gen_audio_1, gen_audio_2, combined_target, output_dir, sr, class_name_1, class_name_2, alpha):
+def save_samples(gen_audio_1, gen_audio_2, combined_target, output_dir, sr, class_name_1, class_name_2, alpha, beta):
     # Save combined target audio
     sf.write(f"{output_dir}/combined_target_audio.wav", combined_target.cpu().numpy(), sr)
     
@@ -49,7 +49,7 @@ def save_samples(gen_audio_1, gen_audio_2, combined_target, output_dir, sr, clas
     for j in range(combined_samples.shape[0]):
         combined_sample = combined_samples[j].cpu()
         combined_sample = high_pass_filter(combined_sample)
-        write(f"{output_dir}/{class_name_1}_{class_name_2}_combined_{str(j+1).zfill(3)}.wav", sr, combined_sample)
+        write(f"{output_dir}/{class_name_1}_{class_name_2}_combined_{alpha}_{beta}_{str(j+1).zfill(3)}.wav", sr, combined_sample)
 
 def main(args):
     os.makedirs(args.output_dir, exist_ok=True)
@@ -82,7 +82,7 @@ def main(args):
     
     # Combine the two audios using the beta parameter
     combined_target = args.beta * target_audio_1 + (1 - args.beta) * target_audio_2
-    combined_target = combined_target.to(device)
+    combined_target = combined_target #.to(device)
     target_event = get_event_cond(combined_target, params['event_type']).repeat(args.N, 1).to(device)
     
     # Generate samples based on new target audio
@@ -91,7 +91,7 @@ def main(args):
     gen_audio_1, gen_audio_2 = generate_samples(target_event, class_idx1, class_idx2, sampler, args.cond_scale, device, args.N, audio_length)
     
     # Save combined target and generated samples
-    save_samples(gen_audio_1, gen_audio_2, combined_target, args.output_dir, sample_rate, args.class_name_1, args.class_name_2, args.alpha)
+    save_samples(gen_audio_1, gen_audio_2, combined_target, args.output_dir, sample_rate, args.class_name_1, args.class_name_2, args.alpha, args.beta)
     print('Done!')
 
 if __name__ == '__main__':
@@ -102,9 +102,9 @@ if __name__ == '__main__':
     parser.add_argument('--target_audio_path_2', type=str, required=True, help='Path to the second target audio file.')
     parser.add_argument('--class_name_1', type=str, required=True, help='First class name for generating samples.', choices=LABELS)
     parser.add_argument('--class_name_2', type=str, required=True, help='Second class name for generating samples.', choices=LABELS)
-    parser.add_argument('--output_dir', type=str, default="./results")
+    parser.add_argument('--output_dir', type=str, default="./results_idea_2")
     parser.add_argument('--cond_scale', type=int, default=3)
-    parser.add_argument('--N', type=int, default=3)
+    parser.add_argument('--N', type=int, default=1)
     parser.add_argument('--alpha', type=float, default=0.5, help='Weighting factor for combining gen_audio_1 and gen_audio_2.')
     parser.add_argument('--beta', type=float, default=0.5, help='Weighting factor for combining target_audio_1 and target_audio_2.')
     args = parser.parse_args()
